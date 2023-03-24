2b8b049dc699a153368941354f2abb4d4c6c3116
ericPinheiro <ericvixi@gmail.com>
2023-03-24 09:26:46 -0300

    add README.md

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ function toggleMode() {
   const img = document.querySelector("#profile img")
   
   if(html.classList.contains('light')) {
-    img.setAttribute('src', './assets/profileLight.png')
-    img.setAttribute('alt', 'Imagem de Eric com camiseta preta')
-  } else {
     img.setAttribute('src', './assets/profileDark.jpg')
     img.setAttribute('alt', 'Imagem de Eric usando óculos escuros')
+  } else {
+    img.setAttribute('src', './assets/profileLight.png')
+    img.setAttribute('alt', 'Imagem de Eric com camiseta preta')
   }
 }
